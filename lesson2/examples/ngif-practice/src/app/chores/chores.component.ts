fae2c325966f1afcee17a514404f993b6c1401d0
Ashley <ashley.horn15@yahoo.com>
2023-03-01 13:39:32 -0600

    BreakFromExamples

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = [];
+   finishedChores = ['Dishwasher emptied', 'LC Prep Work finished', 'Groceries finished'];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   targetImage = 'https://www.launchcode.https://www.applesfromny.chttps://preppykitchen.com/wp-content/uploads/2016/11/Apple-pie-cookie-feature-2.jpgom/wp-content/uploads/2020/06/SnapdragonNEW.pngorg/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
    constructor() { }
 
@@ -17,3 +17,4 @@ export class ChoresComponent implements OnInit {
    }
 
 }
+//'Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'
