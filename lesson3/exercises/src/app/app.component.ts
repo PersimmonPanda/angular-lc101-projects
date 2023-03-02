939d7a44ec5c231628c03b2fb69f56c929ba1cd0
Ashley <ashley.horn15@yahoo.com>
2023-03-02 15:48:57 -0600

    Rocket Mission 31.6 Exercises4

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -8,8 +8,59 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Exercises: Angular Lesson 3';
 
-  color = 'green';
-  height = 0;
-  width = 0;
-  message = 'Space shuttle ready for takeoff!';
+  color: string = 'green';
+  height: number = 0;
+  width: number = 0;
+  message: string = 'Space shuttle ready for takeoff!';
+  statusCheck: boolean = false;
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+    }
+ }
+ handleLanding(image) {
+  let result = window.alert('The shuttle is landing. Landing gear engaged.');
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'The shuttle has landed.';
+     image.style.bottom = '0px' 
 }
+
+handleAbort(image) {
+  let result = window.confirm('Are you sure you want to abort the mission?');
+  if (result) {
+     this.color = 'red';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Mission aborted. This missage will self-destruct in 3...2...1...Goodbye, Ethan *BOOM*.';
+     image.style.bottom = '0px'
+  }
+}
+moveRocket(image, direction: string) {
+  if (direction === 'right') {
+    let movement = parseInt(image.style.left) + 10 + 'px';
+    image.style.left = movement
+    this.width += 10000
+  } else if (direction === 'left') {
+    let movement = parseInt(image.style.left) - 10 + 'px';
+    image.style.left = movement
+    this.width -= 10000
+  } else if (direction === 'up') {
+    let movement = parseInt(image.style.bottom) + 10 + 'px';
+    image.style.bottom = movement
+    this.height += 10000
+  } else {
+    let movement = parseInt(image.style.bottom) - 10 + 'px';
+    image.style.bottom = movement
+    this.height -= 10000
+  }
+}
+
+}
+
